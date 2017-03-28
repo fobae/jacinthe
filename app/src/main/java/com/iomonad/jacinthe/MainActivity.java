d98cd914c36351daa98d2ea2508c70957b9c2d5b
La Monade <iomonad@riseup.net>
2017-03-28 21:12:47 +0200

    Added json class

--- app/src/main/java/com/iomonad/jacinthe/MainActivity.java
+++ app/src/main/java/com/iomonad/jacinthe/MainActivity.java
@@ -2,9 +2,11 @@ package com.iomonad.jacinthe;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.iomonad.jacinthe.api.*;
 
 public class MainActivity extends AppCompatActivity {
 
+    private String TAG = MainActivity.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
